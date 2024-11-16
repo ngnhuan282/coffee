30026dbd26fbcbc43082e6f39ea0a549a9db56a5
hanhut0710 <huynhanhnhut94@gmail.com>
2024-11-16 10:10:18 +0700

    sửaaaaa

--- js/checkout.js
+++ js/checkout.js
@@ -312,8 +312,7 @@ function xulyDathang(product) {
     let giaovaogio = document.querySelector("#deliverytime");
     // Hinh thuc giao & Dia chi nhan hang
     if(giaotannoi.classList.contains("active")) {
-        diachinhan = document.querySelector("#diachinhan").value;
-        diachinhan = innerText;
+        document.querySelector("#diachinhan").value;
         hinhthucgiao = giaotannoi.innerText;
     }
     if(tudenlay.classList.contains("active")){
