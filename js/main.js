5df12c44ab004138ff24ed67992d2254e8781ab9
hanhut0710 <huynhanhnhut94@gmail.com>
2024-12-03 10:41:09 +0700

    upp

--- js/main.js
+++ js/main.js
@@ -842,42 +842,42 @@ window.addEventListener("scroll", () => {
 })
 
 // Page
-function renderProducts(showProduct) {
-    let productHtml = '';
-    if(showProduct.length == 0) {
-        document.getElementById("home-title").style.display = "none";
-        productHtml = `<div class="no-result"><div class="no-result-h">Tìm kiếm không có kết quả</div><div class="no-result-p">Xin lỗi, chúng tôi không thể tìm được kết quả hợp với tìm kiếm của bạn</div><div class="no-result-i"><i class="fa-light fa-face-sad-cry"></i></div></div>`;
-    } else {
-        document.getElementById("home-title").style.display = "block";
-        showProduct.forEach((product) => {
-            productHtml += `<div class="col-product">
-            <article class="card-product" >
-                <div class="card-header">
-                    <a href="#" class="card-image-link" onclick="detailProduct(${product.id})">
-                    <img class="card-image" src="${product.img}" alt="${product.title}">
-                    </a>
-                </div>
-                <div class="food-info">
-                    <div class="card-content">
-                        <div class="card-title">
-                            <a href="#" class="card-title-link" onclick="detailProduct(${product.id})">${product.title}</a>
-                        </div>
-                    </div>
-                    <div class="card-footer">
-                        <div class="product-price">
-                            <span class="current-price">${vnd(product.price)}</span>
-                        </div>
-                    <div class="product-buy">
-                        <button onclick="detailProduct(${product.id})" class="card-button order-item"><i class="fa-regular fa-cart-shopping-fast"></i> Đặt món</button>
-                    </div> 
-                </div>
-                </div>
-            </article>
-        </div>`;
-        });
-    }
-    document.getElementById('home-products').innerHTML = productHtml;
-}
+// function renderProducts(showProduct) {
+//     let productHtml = '';
+//     if(showProduct.length == 0) {
+//         document.getElementById("home-title").style.display = "none";
+//         productHtml = `<div class="no-result"><div class="no-result-h">Tìm kiếm không có kết quả</div><div class="no-result-p">Xin lỗi, chúng tôi không thể tìm được kết quả hợp với tìm kiếm của bạn</div><div class="no-result-i"><i class="fa-light fa-face-sad-cry"></i></div></div>`;
+//     } else {
+//         document.getElementById("home-title").style.display = "block";
+//         showProduct.forEach((product) => {
+//             productHtml += `<div class="col-product">
+//             <article class="card-product" >
+//                 <div class="card-header">
+//                     <a href="#" class="card-image-link" onclick="detailProduct(${product.id})">
+//                     <img class="card-image" src="${product.img}" alt="${product.title}">
+//                     </a>
+//                 </div>
+//                 <div class="food-info">
+//                     <div class="card-content">
+//                         <div class="card-title">
+//                             <a href="#" class="card-title-link" onclick="detailProduct(${product.id})">${product.title}</a>
+//                         </div>
+//                     </div>
+//                     <div class="card-footer">
+//                         <div class="product-price">
+//                             <span class="current-price">${vnd(product.price)}</span>
+//                         </div>
+//                     <div class="product-buy">
+//                         <button onclick="detailProduct(${product.id})" class="card-button order-item"><i class="fa-regular fa-cart-shopping-fast"></i> Đặt món</button>
+//                     </div> 
+//                 </div>
+//                 </div>
+//             </article>
+//         </div>`;
+//         });
+//     }
+//     document.getElementById('home-products').innerHTML = productHtml;
+// }
 
 // Find Product
 var productAll = JSON.parse(localStorage.getItem('products')).filter(item => item.status == 1);
